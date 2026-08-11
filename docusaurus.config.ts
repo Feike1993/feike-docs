ae255c4d9080260bf92e501ee815724f763ba9d6
丰佳 <fengjia@naradapower.com>
2026-08-11 17:24:03 +0800

    feat: 更新网站标题和标语，替换 logo 图像

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,8 +5,8 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: '飞客',
+  tagline: '持续学习,拥抱变化.',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
@@ -75,9 +75,9 @@ const config: Config = {
       respectPrefersColorScheme: true,
     },
     navbar: {
-      title: 'My Site',
+      title: 'Feike',
       logo: {
-        alt: 'My Site Logo',
+        alt: 'Feike Logo',
         src: 'img/logo.svg',
       },
       items: [
